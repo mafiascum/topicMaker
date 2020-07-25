7e46b00d074f34754aece0f1faff53633e478d7b
Isis-maker <68393159+Isis-maker@users.noreply.github.com>
2020-07-25 17:25:51 -0500

    Create common.php

---
+++ language/en/common.php
@@ -0,0 +1,14 @@
+<?php
+/**
+*
+* @package phpBB Extension - MafiaScum ISOs and Activity Overview
+* @copyright (c) 2017 mafiascum.net
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
