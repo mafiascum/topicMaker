78c66c7e684dc86c6d44c7d7e2b120dfbf98e3fe
Isis-maker <68393159+Isis-maker@users.noreply.github.com>
2020-07-31 21:32:55 -0500

    revert

--- event/main_listener.php
+++ event/main_listener.php
@@ -28,7 +28,6 @@ class main_listener implements EventSubscriberInterface
 		return array(
 			'core.posting_modify_cannot_edit_conditions' => 'check_topic_own',
 			'core.user_setup' => 'load_language_on_setup',
-			'core.memberlist_view_profile'			=> 'replace_joined_lang_entry',
 			);
 	}
 	public function __construct(\phpbb\controller\helper $helper, \phpbb\template\template $template, \phpbb\request\request $request, \phpbb\db\driver\driver_interface $db)
@@ -62,23 +61,7 @@ class main_listener implements EventSubscriberInterface
         );
         $event['lang_set_ext'] = $lang_set_ext;
 	}
-		/**
-	 * Replaces 'JOINED' language entry with 'REGISTEREDFOR' one
-	 *
-	 * @param \phpbb\event\data	$event		Event object
-	 * @param string			$eventname	Name of the event
-	 */
-	public function replace_joined_lang_entry($event, $eventname)
-	{
-		$user_id = $event['topic_data']['topic_poster'] ?? $event['member']['user_id'] ?? $event['message_row']['author_id'] ?? ANONYMOUS;
-		if ((int) $user_id != ANONYMOUS)
-		{
-			if ($eventname == 'core.memberlist_view_profile')
-			{
-				$this->language->add_lang('registeredfor', 'rxu/registeredfor');
-			}
-			$this->template->assign_var('L_JOINED', $this->language->lang('REGISTEREDFOR'));
-		}
+	
 	}
 }
 
