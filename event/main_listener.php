6cdf7c52423e23560d9329eb66836e60c57f5144
Isis-maker <68393159+Isis-maker@users.noreply.github.com>
2020-07-31 21:19:15 -0500

    scavengin

--- event/main_listener.php
+++ event/main_listener.php
@@ -17,20 +17,29 @@ class main_listener implements EventSubscriberInterface
 
     /* @var \phpbb\db\driver\driver */
 	protected $db;
+	/** @var \phpbb\user */
+	protected $user;
+	
+	/** @var \phpbb\language\language */
+	protected $language;
 
 	static public function getSubscribedEvents()
 	{
 		return array(
 			'core.posting_modify_cannot_edit_conditions' => 'check_topic_own',
 			'core.user_setup' => 'load_language_on_setup',
+			'core.memberlist_view_profile'			=> 'replace_joined_lang_entry',
 			);
 	}
 	public function __construct(\phpbb\controller\helper $helper, \phpbb\template\template $template, \phpbb\request\request $request, \phpbb\db\driver\driver_interface $db)
 	{
 	    $this->helper = $helper;
-        $this->template = $template;
         $this->request = $request;
         $this->db = $db;
+
+	$this->language = $language;
+		$this->template = $template;
+		$this->user = $user;
 	}
 
 	public function check_topic_own($event)
@@ -44,7 +53,7 @@ class main_listener implements EventSubscriberInterface
 	$s_cannot_edit_locked = ITEM_UNLOCKED;
 	$event['s_cannot_edit_locked'] = $s_cannot_edit_locked;
 	}
-public function load_language_on_setup($event)
+	public function load_language_on_setup($event)
 	{$lang_set_ext = $event['lang_set_ext'];
 	print("<div>The code inside blah blah</div>");
         $lang_set_ext[] = array(
@@ -52,6 +61,24 @@ public function load_language_on_setup($event)
             'lang_set' => 'common',
         );
         $event['lang_set_ext'] = $lang_set_ext;
+	}
+		/**
+	 * Replaces 'JOINED' language entry with 'REGISTEREDFOR' one
+	 *
+	 * @param \phpbb\event\data	$event		Event object
+	 * @param string			$eventname	Name of the event
+	 */
+	public function replace_joined_lang_entry($event, $eventname)
+	{
+		$user_id = $event['topic_data']['topic_poster'] ?? $event['member']['user_id'] ?? $event['message_row']['author_id'] ?? ANONYMOUS;
+		if ((int) $user_id != ANONYMOUS)
+		{
+			if ($eventname == 'core.memberlist_view_profile')
+			{
+				$this->language->add_lang('registeredfor', 'rxu/registeredfor');
+			}
+			$this->template->assign_var('L_JOINED', $this->language->lang('REGISTEREDFOR'));
+		}
 	}
 }
 
