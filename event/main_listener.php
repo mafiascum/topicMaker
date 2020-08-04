c82592969e4ec1f89d75fb935e90ddd8cbe4c3ac
Isis-maker <68393159+Isis-maker@users.noreply.github.com>
2020-08-04 01:58:57 -0500

    Update main_listener.php

--- event/main_listener.php
+++ event/main_listener.php
@@ -7,15 +7,15 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class main_listener implements EventSubscriberInterface
 {
 	/* @var \phpbb\controller\helper */
-    protected $helper;
+    	protected $helper;
 
-    /* @var \phpbb\template\template */
-    protected $template;
+    	/* @var \phpbb\template\template */
+    	protected $template;
 
-    /* @var \phpbb\request\request */
-    protected $request;
+    	/* @var \phpbb\request\request */
+    	protected $request;
 
-    /* @var \phpbb\db\driver\driver */
+    	/* @var \phpbb\db\driver\driver */
 	protected $db;
 	/** @var \phpbb\user */
 	protected $user;
@@ -31,15 +31,16 @@ class main_listener implements EventSubscriberInterface
 			'core.page_header'	=> 'add_page_header_link',
 			);
 	}
+	
 	public function __construct(\phpbb\controller\helper $helper, \phpbb\template\template $template, \phpbb\request\request $request, \phpbb\db\driver\driver_interface $db)
 	{
-	    $this->helper = $helper;
+	$this->helper = $helper;
         $this->request = $request;
         $this->db = $db;
 
 	$this->language = $language;
-		$this->template = $template;
-		$this->user = $user;
+	$this->template = $template;
+	$this->user = $user;
 	}
 
 	public function check_topic_own($event)
@@ -51,6 +52,7 @@ class main_listener implements EventSubscriberInterface
 	$s_cannot_edit_locked = ITEM_UNLOCKED;
 	$event['s_cannot_edit_locked'] = $s_cannot_edit_locked;
 	}
+	
 	public function load_language_on_setup($event)
 	{$lang_set_ext = $event['lang_set_ext'];
 	
@@ -60,6 +62,7 @@ class main_listener implements EventSubscriberInterface
         );
         $event['lang_set_ext'] = $lang_set_ext;
 	}
+	
 	public function add_page_header_link($event)
 	{
 		$this->template->assign_vars(array(
