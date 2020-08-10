f10a35609021b5ffd3bae20cf73517b09abc09b1
Isis-maker <68393159+Isis-maker@users.noreply.github.com>
2020-08-10 11:46:12 -0500

    more plagiarism

--- event/main_listener.php
+++ event/main_listener.php
@@ -41,7 +41,8 @@ class main_listener implements EventSubscriberInterface
 	$this->helper = $helper;
     $this->request = $request;
     $this->db = $db;
-
+	$this->auth = $auth;
+	$this->user = $user;
 	$this->language = $language;
 	$this->template = $template;
 	$this->user = $user;
@@ -71,19 +72,89 @@ class main_listener implements EventSubscriberInterface
 	
 	public function post_edit($event)
 	{
-		print("<div>The code  blah blah</div>");
+		// Are we working on the first post of the topic?
+		$is_first_post = $event['post_data']['topic_first_post_id'] == $event['post_data']['post_id'];
+		$is_author = $event['post_data']['poster_id'] == $this->user->data['user_id'];
+
+		// Time based editing
+		if ($event['s_cannot_edit_time'])
+		{
+			// First post time bypass
+			$allowed_forum = $this->auth->acl_get('f_time_edit_first_post', $event['post_data']['forum_id']);
+			$event['s_cannot_edit_time'] = !($is_first_post && $allowed_forum);
+
+			// Other posts:
+			if (!$is_first_post)
+			{
+				$event['s_cannot_edit_time'] = !$this->auth->acl_get('f_time_edit', $event['post_data']['forum_id']);
+			}
+		}
+
+		// Independent permissions for first post:
+		if ($is_first_post)
+		{
+			$event['s_cannot_edit'] = !($is_author && $this->auth->acl_get('f_edit_first_post', $event['post_data']['forum_id']));
+		}
+		else
+		{
+			// We need to check again for edit permissions because we bypassed that earlier
+			// Ignore data served by event because we changed the way this permission works
+			$event['s_cannot_edit'] = !($is_author && $this->auth->acl_get('f_edit', $event['post_data']['forum_id']));
+		}
 	}
 	public function viewtopic_edit($event)
 	{
-		print("<div>html</div");
+		$is_first_post = $event['topic_data']['topic_first_post_id'] == $event['row']['post_id'];
+		$is_author = $event['row']['user_id'] == $this->user->data['user_id'];
+
+		// Time based editing
+		if ($event['s_cannot_edit_time'])
+		{
+			// First Post
+			$allowed_forum = $this->auth->acl_get('f_time_edit_first_post', $event['row']['forum_id']);
+			$event['s_cannot_edit_time'] = !($is_first_post && $allowed_forum);
+
+			// Other posts
+			if (!$is_first_post)
+			{
+				$event['s_cannot_edit_time'] = !$this->auth->acl_get('f_time_edit', $event['row']['forum_id']);
+			}
+		}
+
+		// Independent permissions for first post:
+		if ($is_first_post)
+		{
+			$event['s_cannot_edit'] = !($is_author && $this->auth->acl_get('f_edit_first_post', $event['row']['forum_id']));
+		}
+		else
+		{
+			// We need to check again for edit permissions because we bypassed that earlier
+			// Ignore data served by event because we changed the way this permission works
+			$event['s_cannot_edit'] = !($is_author && $this->auth->acl_get('f_edit', $event['row']['forum_id']));
+		}
 	}
 	public function add_permissions($event)
 	{
-		print("hallpass");
+		// We redefine f_edit so its new meaning is reflected in the text of the permissions
+		$event['permissions'] = array_merge($event['permissions'], array(
+			// Forum perms
+			'f_edit_first_post'			=> array('lang' => 'ACL_F_FIRST_POST_EDIT', 'cat' => 'post'),
+			'f_time_edit_first_post'	=> array('lang' => 'ACL_F_TIME_FIRST_POST_EDIT', 'cat' => 'post'),
+			'f_time_edit'				=> array('lang' => 'ACL_F_TIME_EDIT', 'cat' => 'post'),
+			'f_edit'					=> array('lang' => 'ACL_F_EDIT_REPLY', 'cat' => 'post'),
+		));
 	}
 	public function post_auth($event)
 	{
-		print("hall monitor");
+		if (!$event['is_authed'] && $event['mode'] == 'edit')
+		{
+			// May still be authed if f_edit_first_post is set
+			$event['is_authed'] = $this->auth->acl_get('f_edit_first_post', $event['forum_id']);
+
+			// May also be authed if f_edit is set
+			// overrule any additional extension run before this one to keep things consistent
+			$event['is_authed'] = $event['is_authed'] || $this->auth->acl_get('f_edit', $event['forum_id']);
+		}
 	}
 }
 
